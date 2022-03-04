5094dd184ed7328e387c12d4a7f8b3c9d9bf2ad7
AndyGreenPhD <agreen57@kennesaw.edu>
2022-03-04 01:15:29 -0500

    more image testing

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image'
+import profilePic from 'images.gus.jpeg'
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
+      <image src={profilePic} alt="Picture of Gus"/>
         <Link href="/">
           <a>{name}</a>
         </Link>
