d2d59527809d0b18c9bc227021d21aa94a9a5230
AndyGreenPhD <agreen57@kennesaw.edu>
2022-03-03 23:53:28 -0500

    fine tuning

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Gus Trupiano';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Gus Trupiano for Oakhurst';
+    : 'HOA President';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
