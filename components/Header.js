81714a651b209281fdd835c27b13c9c022556b96
AndyGreenPhD <agreen57@kennesaw.edu>
2022-03-04 01:20:09 -0500

    moar syntax errors

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image'
-import profilePic from 'images.gus.jpeg'
+import profilePic from 'images/gus.jpeg'
 
 export default function Header({ name }) {
   return (
