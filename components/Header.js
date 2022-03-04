27edff0e8c43414ba14c5d2e9b0581e9bd11b640
AndyGreenPhD <agreen57@kennesaw.edu>
2022-03-04 01:46:47 -0500

    remove gradient

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,6 @@ import profilePic from '../images/gus.jpg'
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
       <Image src={profilePic} width={384} height={512} alt="Picture of Gus"/>
         <Link href="/">
