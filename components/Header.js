ce868b5b1d320c29a12d167ea1caa9c5677f2e2a
AndyGreenPhD <agreen57@kennesaw.edu>
2022-03-04 01:32:34 -0500

    another pic attempt

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image'
+import profilePic from '../images/gus.jpg'
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
-      <Image src="/images/gus.jpg" alt="Picture of Gus"/>
+      <Image src={profilePic} alt="Picture of Gus"/>
         <Link href="/">
           <a>{name}</a>
         </Link>
